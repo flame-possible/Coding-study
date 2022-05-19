bddbca61f9d146fdb736e0ff398118174f3be733
flame-possible <104985055+flame-possible@users.noreply.github.com>
2022-05-19 19:56:33 +0900

    [Gold IV] Title: 오큰수, Time: 596 ms, Memory: 13620 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int stack[1000001] = {0,};
+int nodecnt = 0;
+
+int main(){
+
+        int N;
+
+        cin >> N;
+
+        int input[1000001] = {0,};
+
+        for(register int i = 0; i < N; i++){
+
+                cin >> input[i];
+        }
+
+        int result[1000001] = {0,};
+
+        for(register int i = N-1; i > -1; i--){
+
+                while(nodecnt != 0 && input[i] >= stack[nodecnt-1]){
+                        nodecnt--;
+                }
+
+                if(nodecnt == 0){
+                        result[i] = -1;
+                }
+                else{
+                        result[i] = stack[nodecnt-1];
+                }
+
+                stack[nodecnt++] = input[i];
+
+        }
+
+        for(register int i = 0; i < N; i++){
+                cout << result[i] << ' ';
+        }
+        cout << '\n';
+
+}
