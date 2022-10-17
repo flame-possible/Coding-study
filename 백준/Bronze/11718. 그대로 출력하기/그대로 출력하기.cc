e0d20444f5a4d9b2ce5b8dec8adac000fce0ac73
flame-possible <104985055+flame-possible@users.noreply.github.com>
2022-10-17 17:01:53 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+        char input;
+
+        while(scanf("%c", &input) != EOF){
+                cout << input;
+        }
+
+        return 0;
+
+}
