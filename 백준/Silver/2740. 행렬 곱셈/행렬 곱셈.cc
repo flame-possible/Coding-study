27cb0f6ec2f32941e2e8e679b1df1e6bf1bade06
flame-possible <104985055+flame-possible@users.noreply.github.com>
2022-12-25 20:29:38 +0900

    [Silver V] Title: 행렬 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.cc
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.cc
@@ -1,56 +1,54 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
 
-        register int A_N, A_M;
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
 
-        int A[101][101] = {0,};
+        int N, M, K;
 
-        register int B_N, B_M;
+        int input_1[101][101] = {0,};
+        int input_2[101][101] = {0,};
 
-        int B[101][101] = {0,};
+        cin >> N >> M;
 
-        cin >> A_N >> A_M;
+        for(register int i = 1; i <= N; i++){
 
-        for(register int i = 1; i <= A_N; i++){
-                for(register int j = 1; j <= A_M; j++){
-                        cin >> A[i][j];
+                for(register int j = 1; j <= M; j++){
+                        cin >> input_1[i][j];
                 }
+
         }
 
-        cin >> B_N >> B_M;
+        cin >> M >> K;
 
-        for(register int i = 1; i <= B_N; i++){
-                for(register int j = 1; j <= B_M; j++){
-                        cin >> B[i][j];
+        for(register int i = 1; i <= M; i++){
+
+                for(register int j = 1; j <= K; j++){
+                        cin >> input_2[i][j];
                 }
+
         }
 
         int result[101][101] = {0,};
 
-        for(register int i = 1; i <= A_N; i++){
+        for(register int i = 1; i <= N; i++){
 
-                for(register int j = 1; j <= B_M; j++){
+                for(register int j = 1; j <= K; j++){
 
-                        register int temp_result = 0;
-                        register int temp[101] = {0,};
-
-                        for(register int k = 1; k <= A_M; k++){
-                                temp[k] = A[i][k];
-                        }
-                        for(register int k = 1; k <= B_N; k++){
-                                temp[k] *= B[k][j];
-                                temp_result += temp[k];
+                        for(register int k = 1; k <= M; k++){
+                                result[i][j] += input_1[i][k] * input_2[k][j];
                         }
 
-                        result[i][j] = temp_result;
                         cout << result[i][j] << ' ';
-                }
-                cout << '\n';
-        }
 
+                }
+
+                cout << '\n';
+
+        }
 
 
         return 0;
