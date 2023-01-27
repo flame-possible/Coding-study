6126cdfcfb9c5054e635ba9b64d93198bd265748
flame-possible <104985055+flame-possible@users.noreply.github.com>
2023-01-27 21:29:53 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+
+        char input[101];
+
+        cin >> input;
+
+        int result = strlen(input);
+
+        cout << result << '\n';
+
+        return 0;
+
+}
