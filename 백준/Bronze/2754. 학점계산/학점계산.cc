7859e0f8fb9d344c97bb4e4fb4ec28b7c27de831
flame-possible <104985055+flame-possible@users.noreply.github.com>
2023-02-11 19:57:29 +0900

    [Bronze V] Title: 학점계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+
+        string input;
+
+        cin >> input;
+
+        if(input == "A+") cout << "4.3\n";
+        if(input == "A0") cout << "4.0\n";
+        if(input == "A-") cout << "3.7\n";
+        if(input == "B+") cout << "3.3\n";
+        if(input == "B0") cout << "3.0\n";
+        if(input == "B-") cout << "2.7\n";
+        if(input == "C+") cout << "2.3\n";
+        if(input == "C0") cout << "2.0\n";
+        if(input == "C-") cout << "1.7\n";
+        if(input == "D+") cout << "1.3\n";
+        if(input == "D0") cout << "1.0\n";
+        if(input == "D-") cout << "0.7\n";
+        if(input == "F") cout << "0.0\n";
+
+
+        return 0;
+
+}
