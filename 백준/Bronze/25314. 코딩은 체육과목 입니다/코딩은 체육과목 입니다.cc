0c1d395c648406aa77b1385c6d8dd44ee93691e7
flame-possible <104985055+flame-possible@users.noreply.github.com>
2023-02-24 19:23:37 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+
+        int N;
+
+        cin >> N;
+
+        int result = N / 4;
+
+        for(register int i = 0; i < result - 1; i++){
+                cout << "long ";
+        }
+
+        cout << "long int\n";
+
+
+        return 0;
+
+}
